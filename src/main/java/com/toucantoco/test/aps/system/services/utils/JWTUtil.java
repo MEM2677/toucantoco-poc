463c86853a2f01251095b8e800a39158032c415a
Matteo Emanuele M <entfiannelli@cdp.it>
2018-09-14 19:55:49 +0200

    Initial commit (JWT generation)

---
+++ src/main/java/com/toucantoco/test/aps/system/services/utils/JWTUtil.java
@@ -0,0 +1,21 @@
+package com.toucantoco.test.aps.system.services.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+public class JWTUtil {
+
+	public static String generateToucanTocoJWT(String secret, String username) throws Throwable {
+		Algorithm algorithm = Algorithm.HMAC256(secret);
+		
+		return generateJWT(algorithm, username);
+	}
+	
+	public static String generateJWT(Algorithm algorithm, String username) {
+		String token = JWT.create()
+				.withSubject(username)
+				.sign(algorithm);
+		return token;
+	}
+	
+}
